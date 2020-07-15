42ce94698167f66c69d218b1aabade085781bd0c
NaiWuPu <15542848579@163.com>
2020-07-15 10:22:29 +0800

    Default Changelist

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-##kafka 消费端日志
-#####推送入ES
+## kafka 消费端日志 推送入ES
+### 未使用 influshDB 原因是 influshDB在一定规模之后必须要使用企业版
+### ElasticSearch 及 kibana 安装 https://www.elastic.co/cn/downloads/
 
-###未使用 influshDB 原因是 influshDB在一定规模之后必须要使用企业版
+######### 目录结构描述
+``` 
+├─.idea
+├─conf                          // 配置文件
+├─es                            // 初始化Es 暴露写入Es 接口
+├─kafka                         // 初始化kafka 并开启暴露
+└─vendor                        // 三方包
+```
 
-##ElasticSearch 及 kibana 安装 https://www.elastic.co/cn/downloads/
+#### 运行 go run main.go
--- main.go
+++ main.go
@@ -6,10 +6,10 @@ import (
 	"log_transfer/conf"
 	"log_transfer/es"
 	"log_transfer/kafka"
-	"log_transfer/vendor/gopkg.in/ini.v1"
 )
+
 // 将日志数据从kafka 取出来发往ES
-func main()  {
+func main() {
 	// 加载配置文件
 	var cfg = new(conf.LogTransfer)
 	err := ini.MapTo(cfg, "./conf/cfg.ini")
@@ -17,7 +17,7 @@ func main()  {
 		fmt.Printf("init config failed:%v \n", err)
 		return
 	}
-	fmt.Printf("cfg:%v\n",cfg)
+	fmt.Printf("cfg:%v\n", cfg)
 	// 初始化ES
 	// 初始化一个ES的链接 client
 	err = es.Init(cfg.ESCfg.Address, cfg.ESCfg.ChanSize, cfg.ESCfg.Nums)
@@ -32,8 +32,6 @@ func main()  {
 		return
 	}
 
-	select {
-
-	}
+	select {}
 
 }
